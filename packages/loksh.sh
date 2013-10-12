185ad093244fba22356fce43222738ce825ee31b
Dima Krasner <dima@dimakrasner.com>
2013-10-12 12:36:15 +0000

    Replaced DASH and tcsh with loksh.

---
+++ packages/loksh.sh
@@ -0,0 +1,20 @@
+PACKAGE_VERSION="master"
+PACKAGE_SOURCES="https://github.com/iguleder/loksh/archive/master.zip,loksh-master.zip"
+
+loksh_build() {
+	[ -d loksh-master ] && rm -rf loksh-master
+	unzip loksh-master.zip
+	cd loksh-master
+
+	$CC $CFLAGS -I. *.c -o loksh
+}
+
+loksh_package() {
+	install -D -m 755 loksh "$1/bin/loksh"
+	install -D -m 644 ksh.1 "$1/usr/share/man/man1/loksh.1"
+	install -D -m 644 README "$1/usr/share/doc/loksh/README"
+	install -m 644 ChangeLog "$1/usr/share/doc/loksh/ChangeLog"
+	install -m 644 ChangeLog.0 "$1/usr/share/doc/loksh/ChangeLog.0"
+	install -m 644 CONTRIBUTORS "$1/usr/share/doc/loksh/CONTRIBUTORS"
+	install -m 644 LEGAL "$1/usr/share/doc/loksh/LEGAL"
+}
