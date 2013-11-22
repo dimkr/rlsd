7dabf31beb593d26bd36fe5bb165baa65a2159ae
Dima Krasner <dima@dimakrasner.com>
2013-11-22 17:43:46 +0000

    Added emelFM and Beaver.

---
+++ packages/beaver.sh
@@ -0,0 +1,28 @@
+PACKAGE_VERSION="0.2.6"
+PACKAGE_SOURCES="http://download.savannah.gnu.org/releases/beaver/archive/beaver-$PACKAGE_VERSION.tar.gz"
+
+
+beaver_build() {
+	[ -d beaver-$PACKAGE_VERSION ] && rm -rf beaver-$PACKAGE_VERSION
+	tar -xzvf beaver-$PACKAGE_VERSION.tar.gz
+	cd beaver-$PACKAGE_VERSION/src
+
+	sed -e s~'CC      = .*'~"CC      = $CC"~ \
+	    -e s~'DESTDIR = .*'~'DESTDIR ?= /'~ \
+	    -e s~'OPTI    = .*'~"OPTI    = $CFLAGS"~ \
+	    -e s~'LDFLAGS = '~"LDFLAGS = $LDFLAGS"~ \
+	    -e s~'\$(DESTDIR)/share'~'$(DESTDIR)/usr/share'~g \
+	    -e s~'\$(DESTDIR)/man'~'$(DESTDIR)/usr/share/man'~g \
+	    -i Makefile
+	make
+}
+
+beaver_package() {
+	make DESTDIR="$1" install
+	install -D -m 644 ../README "$1/usr/share/doc/beaver/README"
+	install -m 644 ../NEWS "$1/usr/share/doc/beaver/NEWS"
+	install -m 644 ../ChangeLog "$1/usr/share/doc/beaver/ChangeLog"
+	install -m 644 ../AUTHORS "$1/usr/share/doc/beaver/AUTHORS"
+	install -m 644 ../THANKS "$1/usr/share/doc/beaver/THANKS"
+	install -m 644 ../COPYING "$1/usr/share/doc/beaver/COPYING"
+}
---
+++ packages/emelfm.sh
@@ -0,0 +1,28 @@
+PACKAGE_VERSION="0.9.2"
+PACKAGE_SOURCES="http://emelfm.sourceforge.net/emelfm-$PACKAGE_VERSION.tar.gz"
+
+emelfm_build() {
+	[ -d emelfm-$PACKAGE_VERSION ] && rm -rf emelfm-$PACKAGE_VERSION
+	tar -xzvf emelfm-$PACKAGE_VERSION.tar.gz
+	cd emelfm-$PACKAGE_VERSION
+
+	sed -e s~'CC = .*'~"CC = $CC"~ \
+	    -e s~'PREFIX = .*'~'PREFIX = '~ \
+	    -e s~'PLUGINS_DIR = .*'~'PLUGINS_DIR = $(PREFIX)/lib/emelfm/plugins'~ \
+	    -e s~'DOC_DIR = .*'~'DOC_DIR = $(PREFIX)/usr/share/doc/emelfm'~ \
+	    -e s~'NLS = -DENABLE_NLS'~'NLS ='~ \
+	    -i Makefile.common
+
+	sed -e s~'emelfm plugins nls'~'emelfm nls'~ \
+	    -e s~'cd plugins; make install'~'# &'~ \
+	    -i Makefile
+
+	make
+}
+
+emelfm_package() {
+	install -D -m 755 emelfm "$1/bin/emelfm"
+	install -D -m 644 README "$1/usr/share/doc/emelfm/README"
+	install -m 644 docs/help.txt "$1/usr/share/doc/emelfm/docs/help.txt"
+	install -m 644 COPYING "$1/usr/share/doc/emelfm/COPYING"
+}
