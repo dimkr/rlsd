43ed05ea8e67621d41c01acbae550193f834a41e
Dima Krasner <dima@dimakrasner.com>
2013-11-30 19:20:19 +0000

    Added gtkfontsel.

---
+++ packages/gtkfontsel.sh
@@ -0,0 +1,22 @@
+PACKAGE_VERSION="1.1"
+PACKAGE_SOURCES="http://distro.ibiblio.org/amigolinux/download/Applications/Graphics/gtkfontsel-$PACKAGE_VERSION/gtkfontsel-$PACKAGE_VERSION.tar.bz2"
+
+gtkfontsel_build() {
+	[ -d gtkfontsel-$PACKAGE_VERSION ] && rm -rf gtkfontsel-$PACKAGE_VERSION
+	tar -xjvf gtkfontsel-$PACKAGE_VERSION.tar.bz2
+	cd gtkfontsel-$PACKAGE_VERSION
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --datadir=/usr/share
+	make
+}
+
+gtkfontsel_package() {
+	make DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/gtkfontsel/README"
+	install -m 644 NEWS "$1/usr/share/doc/gtkfontsel/NEWS"
+	install -m 644 ChangeLog "$1/usr/share/doc/gtkfontsel/ChangeLog"
+	install -m 644 AUTHORS "$1/usr/share/doc/gtkfontsel/AUTHORS"
+	install -m 644 COPYING "$1/usr/share/doc/gtkfontsel/COPYING"
+}
