3c3aab43f343d090bd6c6119f8c8a6819ed72d4f
Dima Krasner <dima@dimakrasner.com>
2013-11-30 19:00:08 +0000

    Added mhWaveEdit.

---
+++ packages/mhwaveedit.sh
@@ -0,0 +1,38 @@
+PACKAGE_VERSION="1.4.23"
+PACKAGE_SOURCES="http://download.gna.org/mhwaveedit/mhwaveedit-$PACKAGE_VERSION.tar.bz2"
+
+mhwaveedit_build() {
+	[ -d mhwaveedit-$PACKAGE_VERSION ] && rm -rf mhwaveedit-$PACKAGE_VERSION
+	tar -xjvf mhwaveedit-$PACKAGE_VERSION.tar.bz2
+	cd mhwaveedit-$PACKAGE_VERSION
+
+	patch -p1 < "$BASE_DIR/patches/mhwaveedit-musl.patch"
+	CFLAGS="-D_POSIX_C_SOURCE=200809L $CFLAGS" \
+	./configure --host=$HOST \
+	            --prefix= \
+	            --datarootdir=/usr/share \
+	            --disable-nls \
+	            --disable-gtk2 \
+	            --disable-ladspa \
+	            --with-default-driver=alsa \
+	            --with-default-mixerapp="aterm -e alsamixer" \
+	            --without-libsndfile \
+	            --without-libsamplerate \
+	            --without-oss \
+	            --without-jack \
+	            --without-pulse \
+	            --without-esound \
+	            --without-sun \
+	            --without-arts
+	make
+}
+
+mhwaveedit_package() {
+	make DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/mhwaveedit/README"
+	install -m 644 NEWS "$1/usr/share/doc/mhwaveedit/NEWS"
+	install -m 644 ChangeLog "$1/usr/share/doc/mhwaveedit/ChangeLog"
+	install -m 644 AUTHORS "$1/usr/share/doc/mhwaveedit/AUTHORS"
+	install -m 644 copynotice "$1/usr/share/doc/mhwaveedit/copynotice"
+	install -m 644 COPYING "$1/usr/share/doc/mhwaveedit/COPYING"
+}
