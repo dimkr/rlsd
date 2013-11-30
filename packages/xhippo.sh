c089a4a455566f9b862adc353ab2311ff41ab4a6
Dima Krasner <dima@dimakrasner.com>
2013-11-30 19:48:28 +0000

    Added xhippo.

---
+++ packages/xhippo.sh
@@ -0,0 +1,23 @@
+PACKAGE_VERSION="3.5"
+PACKAGE_SOURCES="ftp://ftp.gnu.org/gnu/xhippo/xhippo-$PACKAGE_VERSION.tar.gz"
+
+xhippo_build() {
+	[ -d xhippo-$PACKAGE_VERSION ] && rm -rf xhippo-$PACKAGE_VERSION
+	tar -xzvf xhippo-$PACKAGE_VERSION.tar.gz
+	cd xhippo-$PACKAGE_VERSION
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --infodir=/usr/share/info \
+	            --mandir=/usr/share/man
+	make
+}
+
+xhippo_package() {
+	make DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/xhippo/README"
+	install -m 644 NEWS "$1/usr/share/doc/xhippo/NEWS"
+	install -m 644 ChangeLog "$1/usr/share/doc/xhippo/ChangeLog"
+	install -m 644 AUTHORS "$1/usr/share/doc/xhippo/AUTHORS"
+	install -m 644 COPYING "$1/usr/share/doc/xhippo/COPYING"
+}
