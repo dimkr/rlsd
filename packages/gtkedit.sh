4b44a2aca0c43bc2ac81fcad0921a6b8c8fff148
Dima Krasner <dima@dimakrasner.com>
2013-12-05 21:14:28 +0000

    Added gtkedit.

---
+++ packages/gtkedit.sh
@@ -0,0 +1,16 @@
+PACKAGE_VERSION="1.0"
+PACKAGE_SOURCES="http://downloads.sourceforge.net/project/gtkedit1/gtkedit1/$PACKAGE_VERSION/gtkedit-$PACKAGE_VERSION-src.tar.gz"
+
+gtkedit_build() {
+	[ -d gtkedit-$PACKAGE_VERSION ] && rm -rf gtkedit-$PACKAGE_VERSION
+	tar -xzvf gtkedit-$PACKAGE_VERSION-src.tar.gz
+	cd gtkedit-$PACKAGE_VERSION-src
+
+	$CC $CFLAGS $(gtk-config --cflags) gtkedit.c $(gtk-config --libs) -o gtkedit
+}
+
+gtkedit_package() {
+	install -D -m 755 gtkedit "$1/bin/gtkedit"
+	install -D -m 644 readme.html "$1/usr/share/doc/gtkedit/readme.html"
+	install -m 644 LICENSE.txt "$1/usr/share/doc/gtkedit/LICENSE.txt"
+}
