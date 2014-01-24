6d59c90dd99b3392bf14e73a33c862860c0ae53c
Dima Krasner <dima@dimakrasner.com>
2014-01-25 01:36:37 +0200

    Implemented a two-stage boot process and a layered file system.
    In the future, the tmpfs file system used as the writable layer can be replaced
    with a "save file" (as in Puppy Linux) or the boot partition.

--- create_iso.sh
+++ create_iso.sh
@@ -2,20 +2,34 @@
 
 . ./config
 
+INITRAMFS_PROGRAMS="init loksh syslogd logd mkdir mount umount luufs chroot"
 BASE_DIR="$(pwd)"
 VERSION="$(date +%d%m%Y)"
 ISO_NAME="lazyux-$VERSION.iso"
 
-# create a temporary directory
+# create a temporary directory for the initramfs contents
+initramfs_root="$(mktemp -d -u)"
+
+# copy the initramfs skeleton
+cp -r initramfs-skeleton "$initramfs_root"
+
+# add required programs to the initramfs
+for i in $INITRAMFS_PROGRAMS
+do
+	cp "$SYSROOT/bin/$i" "$initramfs_root/bin"
+done
+
+# create a temporary directory for the extracted ISO image contents
 iso_root="$(mktemp -d)"
 
-# add the skeleton and generate a compressed initramfs
-cp -ar skeleton/* "$SYSROOT"
-cd "$SYSROOT"
+# generate a compressed initramfs
+cd "$initramfs_root"
+find -name .gitignore -delete
 find . | cpio -o -H newc | xz -9 -e --check=none > "$iso_root/initrd.xz"
+rm -rf "$initramfs_root"
 
-# create a 20 MB, FAT12 UEFI boot image, for UEFI boot
-dd if=/dev/zero of="$iso_root/efiboot.img" bs=1024K count=20
+# create a 4 MB, FAT12 UEFI boot image, for UEFI boot
+dd if=/dev/zero of="$iso_root/efiboot.img" bs=1024K count=4
 mkfs.vfat -F 12 -n "Lazyux" "$iso_root/efiboot.img"
 mount_point="$(mktemp -d)"
 mount -o loop -t vfat "$iso_root/efiboot.img" "$mount_point"
@@ -31,6 +45,19 @@ rmdir "$mount_point"
 cp "$SYSROOT/boot/bzImage" "$iso_root/"
 cp "$SYSROOT/boot/isolinux.bin" "$iso_root/"
 cp "$SYSROOT/boot/isolinux.cfg" "$iso_root/"
+
+# add the skeleton to the root file system
+cd "$BASE_DIR"
+cp -ar skeleton/* "$SYSROOT"
+
+# generate the root file system image
+find "$SYSROOT" -name .gitignore -delete
+mksquashfs "$SYSROOT" \
+           "$iso_root/rootfs.sfs" \
+           -comp xz \
+           -Xbcj x86 \
+           -b 512K \
+           -no-exports
 
 # generate an ISO image
 xorriso -dev "$BASE_DIR/$ISO_NAME" \
