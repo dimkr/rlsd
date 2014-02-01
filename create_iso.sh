0cdd6627033df47251cf721ca27193d288b3c607
Dima Krasner <dima@dimakrasner.com>
2014-02-01 20:53:23 +0200

    Removed the kernel and the boot loader from the root file system.

--- create_iso.sh
+++ create_iso.sh
@@ -31,46 +31,50 @@ find -name .gitignore -delete
 find . | cpio -o -H newc | xz -9 -e --check=none > "$iso_root/initrd.xz"
 rm -rf "$initramfs_root"
 
+# add the skeleton to the root file system
+root_fs="$(mktemp -d -u)"
+cp -ar "$SYSROOT" "$root_fs"
+cp -ar skeleton/* "$root_fs"
+
 # create a 4 MB, FAT12 UEFI boot image, for UEFI boot
 dd if=/dev/zero of="$iso_root/efiboot.img" bs=1024K count=4
-mkfs.vfat -F 12 -n "Lazyux" "$iso_root/efiboot.img"
+mkfs.vfat -F 12 -n "LAZYUX" "$iso_root/efiboot.img"
 mount_point="$(mktemp -d)"
 mount -o loop -t vfat "$iso_root/efiboot.img" "$mount_point"
 mkdir -p "$mount_point/EFI/BOOT"
-cp "$SYSROOT/boot/elilo.efi" "$mount_point/EFI/BOOT/BOOTX64.EFI"
-cp "$SYSROOT/boot/elilo.conf" "$mount_point/EFI/BOOT/"
-cp "$SYSROOT/boot/bzImage" "$mount_point/EFI/BOOT/"
+mv "$root_fs/boot/elilo.efi" "$mount_point/EFI/BOOT/BOOTX64.EFI"
+mv "$root_fs/boot/elilo.conf" "$mount_point/EFI/BOOT/"
+cp "$root_fs/boot/bzImage" "$mount_point/EFI/BOOT/"
 cp "$iso_root/initrd.xz" "$mount_point/EFI/BOOT/"
 umount "$mount_point"
 rmdir "$mount_point"
 
-# copy the kernel, the boot loader and its configuration file, for BIOS boot
-cp "$SYSROOT/boot/bzImage" "$iso_root/"
-cp "$SYSROOT/boot/isolinux.bin" "$iso_root/"
-cp "$SYSROOT/boot/isolinux.cfg" "$iso_root/"
-
-# add the skeleton to the root file system
-cd "$BASE_DIR"
-cp -ar skeleton/* "$SYSROOT"
+# put the kernel, the boot loader and its configuration file in the image root,
+# for BIOS boot
+mv "$root_fs/boot/bzImage" "$iso_root/"
+mv "$root_fs/boot/isolinux.bin" "$iso_root/"
+mv "$root_fs/boot/isolinux.cfg" "$iso_root/"
 
 # generate the root file system image
-find "$SYSROOT" -name .gitignore -delete
-mksquashfs "$SYSROOT" \
+find "$root_fs" -name .gitignore -delete
+mksquashfs "$root_fs" \
            "$iso_root/rootfs.sfs" \
            -comp xz \
            -Xbcj x86 \
            -b 512K \
            -no-exports
+rm -rf "$root_fs"
 
 # generate an ISO image
 xorriso -dev "$BASE_DIR/$ISO_NAME" \
-        -volid "Lazyux $VERSION" \
+        -volid "LAZYUX_$VERSION" \
         -joliet on \
         -compliance iso_9660_level=3 \
         -map "$iso_root" / \
         -boot_image isolinux dir=/ \
         -boot_image isolinux next \
         -boot_image any efi_path=efiboot.img
+isohybrid -u "$BASE_DIR/$ISO_NAME"
 
 # clean up
 rm -rf "$iso_root"
