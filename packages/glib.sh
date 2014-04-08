e42c2ae6a9feb77fd7f7c2e59292ae7f99227ed3
Dima Krasner <dima@dimakrasner.com>
2014-04-08 17:56:25 +0300

    Forked GLib and merged all patches.

--- packages/glib.sh
+++ packages/glib.sh
@@ -1,17 +1,10 @@
-PACKAGE_VERSION="1.2.10"
-PACKAGE_SOURCES="http://ftp.gnome.org/pub/gnome/sources/glib/1.2/glib-$PACKAGE_VERSION.tar.gz https://projects.archlinux.org/svntogit/packages.git/plain/trunk/glib1-autotools.patch?h=packages/glib,glib-autotools.patch https://projects.archlinux.org/svntogit/packages.git/plain/trunk/gcc340.patch?h=packages/glib,glib-gcc340.patch https://projects.archlinux.org/svntogit/packages.git/plain/trunk/aclocal-fixes.patch?h=packages/glib,glib-aclocal-fixes.patch"
+PACKAGE_VERSION="git$(date +%d%m%Y)"
+PACKAGE_SOURCES="https://github.com/iguleder/glib/archive/master.zip,glib-$PACKAGE_VERSION.zip"
 
 glib_build() {
-	[ -d glib-$PACKAGE_VERSION ] && rm -rf glib-$PACKAGE_VERSION
-	tar -xzvf glib-$PACKAGE_VERSION.tar.gz
-	cd glib-$PACKAGE_VERSION
-
-	patch -p0 < ../glib-aclocal-fixes.patch
-	patch -p1 < ../glib-autotools.patch
-	patch -p1 < ../glib-gcc340.patch
-	patch -p0 < "$BASE_DIR/patches/glib-1.2.10-gstrfuncs.diff"
-	patch -p1 < "$BASE_DIR/patches/glib-1.2.10-musl.patch"
-	patch -p1 < "$BASE_DIR/patches/glib-config.patch"
+        [ -d glib-master ] && rm -rf glib-master
+        unzip glib-$PACKAGE_VERSION.zip
+        cd glib-master
 
 	autoreconf --force --install
 	CFLAGS="$CFLAGS -D_LARGEFILE64_SOURCE" \
