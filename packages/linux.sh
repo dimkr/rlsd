c88552647295e67e75055155064c5dba784d75f0
Dima Krasner <dima@dimakrasner.com>
2014-05-04 01:13:10 +0300

    Updated Linux.

--- packages/linux.sh
+++ packages/linux.sh
@@ -1,4 +1,4 @@
-PACKAGE_VERSION="3.10.37"
+PACKAGE_VERSION="3.10.38"
 PACKAGE_MAJOR_VERSION="3.10"
 PACKAGE_SOURCES="http://linux-libre.fsfla.org/pub/linux-libre/releases/$PACKAGE_VERSION-gnu/linux-libre-$PACKAGE_VERSION-gnu.tar.xz"
 
