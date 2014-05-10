a863f955752b24ac20db04168484ac61bfd77ca8
Dima Krasner <dima@dimakrasner.com>
2014-05-10 12:43:00 +0300

    Added Netcat.

---
+++ packages/nc.sh
@@ -0,0 +1,18 @@
+PACKAGE_VERSION="110"
+PACKAGE_SOURCES="http://downloads.sourceforge.net/project/nc110/unix%20netcat%201.10%20by%20_Hobbit_/%5BUnnamed%20release%5D/nc$PACKAGE_VERSION.tgz"
+
+nc_build() {
+	[ -d nc-$PACKAGE_VERSION ] && rm -rf nc-$PACKAGE_VERSION
+	mkdir nc-$PACKAGE_VERSION
+	cd nc-$PACKAGE_VERSION
+	tar -xzvf ../nc$PACKAGE_VERSION.tgz
+
+	patch -p 1 < "$BASE_DIR/patches/nc-build.patch"
+	$MAKE linux LD="$CC"
+}
+
+nc_package() {
+	install -D -m 755 nc "$1/bin/nc"
+	install -D -m 644 README "$1/usr/share/doc/nc/README"
+	install -m 644 Changelog "$1/usr/share/doc/nc/Changelog"
+}
