fde545a899e83578f5b44f9da0316d594e510e30
Dima Krasner <dima@dimakrasner.com>
2014-05-14 00:32:33 +0300

    Added FFmpeg.

---
+++ packages/ffmpeg.sh
@@ -0,0 +1,38 @@
+PACKAGE_VERSION="2.2.2"
+PACKAGE_SOURCES="http://www.ffmpeg.org/releases/ffmpeg-$PACKAGE_VERSION.tar.bz2"
+
+ffmpeg_build() {
+	[ -d ffmpeg-$PACKAGE_VERSION ] && rm -rf ffmpeg-$PACKAGE_VERSION
+	tar -xjvf ffmpeg-$PACKAGE_VERSION.tar.bz2
+	cd ffmpeg-$PACKAGE_VERSION
+
+	./configure --prefix=/usr \
+	            --bindir=/bin \
+	            --libdir=/lib \
+	            --shlibdir=/lib \
+	            $CONFIGURE_LIBRARY_FLAGS \
+	            --enable-small \
+	            --disable-ffplay \
+	            --disable-ffserver \
+	            --disable-network \
+	            --cc="$CC" \
+	            --ld="$CC" \
+	            --cpu="$ARCH" \
+	            --extra-cflags="-D_POSIX_SOURCE $CFLAGS" \
+	            --extra-ldflags="$LDFLAGS" \
+	            --disable-debug
+	$MAKE
+}
+
+ffmpeg_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/ffmpeg/README"
+	install -m 644 LICENSE "$1/usr/share/doc/ffmpeg/LICENSE"
+	install -m 644 COPYING.GPLv2 "$1/usr/share/doc/ffmpeg/COPYING.GPLv2"
+	install -m 644 COPYING.GPLv3 "$1/usr/share/doc/ffmpeg/COPYING.GPLv3"
+	install -m 644 COPYING.LGPLv2.1 "$1/usr/share/doc/ffmpeg/COPYING.LGPLv2.1"
+	install -m 644 COPYING.LGPLv3 "$1/usr/share/doc/ffmpeg/COPYING.LGPLv3"
+	install -m 644 Changelog "$1/usr/share/doc/ffmpeg/Changelog"
+	install -m 644 CREDITS "$1/usr/share/doc/ffmpeg/CREDITS"
+	install -m 644 MAINTAINERS "$1/usr/share/doc/ffmpeg/MAINTAINERS"
+}
