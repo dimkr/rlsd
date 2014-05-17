d5ed5648461f2d31737ea8c1163a294f9e0c149f
Dima Krasner <dima@dimakrasner.com>
2014-05-17 13:13:15 +0300

    Trimmed the terminfo database.
    This makes the ncurses package smaller by ~40%, without any impact on functionality. The removed entries belong to terminals that have nothing to do with Lazyux.

--- packages/ncurses.sh
+++ packages/ncurses.sh
@@ -41,6 +41,101 @@ ncurses_package() {
 	mv "$1/usr/lib/pkgconfig" "$1/lib/"
 	rmdir "$1/usr/lib"
 	ln -s libncurses.a "$1/lib/libtinfo.a"
+
+	# trim the terminfo directory
+	rm -rf "$1/usr/share/terminfo/1" \
+	       "$1/usr/share/terminfo/2" \
+	       "$1/usr/share/terminfo/3" \
+	       "$1/usr/share/terminfo/4" \
+	       "$1/usr/share/terminfo/5" \
+	       "$1/usr/share/terminfo/6" \
+	       "$1/usr/share/terminfo/7" \
+	       "$1/usr/share/terminfo/8" \
+	       "$1/usr/share/terminfo/9" \
+	       "$1/usr/share/terminfo/A" \
+	       "$1/usr/share/terminfo/E" \
+	       "$1/usr/share/terminfo/L" \
+	       "$1/usr/share/terminfo/M" \
+	       "$1/usr/share/terminfo/N" \
+	       "$1/usr/share/terminfo/P" \
+	       "$1/usr/share/terminfo/Q" \
+	       "$1/usr/share/terminfo/X" \
+	       "$1/usr/share/terminfo/a" \
+	       "$1/usr/share/terminfo/b" \
+	       "$1/usr/share/terminfo/c" \
+	       "$1/usr/share/terminfo/d" \
+	       "$1/usr/share/terminfo/e" \
+	       "$1/usr/share/terminfo/f" \
+	       "$1/usr/share/terminfo/g" \
+	       "$1/usr/share/terminfo/h" \
+	       "$1/usr/share/terminfo/i" \
+	       "$1/usr/share/terminfo/j" \
+	       "$1/usr/share/terminfo/k" \
+	       "$1/usr/share/terminfo/e" \
+	       "$1/usr/share/terminfo/m" \
+	       "$1/usr/share/terminfo/n" \
+	       "$1/usr/share/terminfo/o" \
+	       "$1/usr/share/terminfo/p" \
+	       "$1/usr/share/terminfo/q" \
+	       "$1/usr/share/terminfo/t" \
+	       "$1/usr/share/terminfo/u" \
+	       "$1/usr/share/terminfo/w" \
+	       "$1/usr/share/terminfo/z"
+	for i in "$1/usr/share/terminfo/s"/*
+	do
+		case "$i" in
+			*screen*)
+				;;
+			*)
+				rm -f "$i"
+				;;
+		esac
+	done
+
+	for i in "$1/usr/share/terminfo/r"/*
+	do
+		case "$i" in
+			*rxvt*)
+				;;
+			*)
+				rm -f "$i"
+				;;
+		esac
+	done
+
+	for i in "$1/usr/share/terminfo/v"/*
+	do
+		case "$i" in
+			*vt[0-9]*)
+				;;
+			*)
+				rm -f "$i"
+				;;
+		esac
+	done
+
+	for i in "$1/usr/share/terminfo/l"/*
+	do
+		case "$i" in
+			*linux*)
+				;;
+			*)
+				rm -f "$i"
+				;;
+		esac
+	done
+
+	for i in "$1/usr/share/terminfo/x"/*
+	do
+		case "$i" in
+			*xterm*)
+				;;
+			*)
+				rm -f "$i"
+				;;
+		esac
+	done
+
 	install -D -m 644 README "$1/usr/share/doc/ncurses/README"
 	install -D -m 644 NEWS "$1/usr/share/doc/ncurses/NEWS"
 	install -D -m 644 ANNOUNCE "$1/usr/share/doc/ncurses/ANNOUNCE"
