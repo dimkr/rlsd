a6c901a6e0a5484e506cc547919a013d5ccc2f8c
Dima Krasner <dima@dimakrasner.com>
2014-06-12 20:59:12 +0300

    Added file.
    This makes ROX-Filer much more intuitive, since file types are recognized properly.

---
+++ packages/file.sh
@@ -0,0 +1,24 @@
+PACKAGE_VERSION="5.18"
+PACKAGE_SOURCES="ftp://ftp.astron.com/pub/file/file-$PACKAGE_VERSION.tar.gz"
+PACKAGE_DESC="A file type guessing tool"
+
+file_build() {
+	[ -d file-$PACKAGE_VERSION ] && rm -rf file-$PACKAGE_VERSION
+	tar -xzvf file-$PACKAGE_VERSION.tar.gz
+	cd file-$PACKAGE_VERSION
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --includedir=/usr/include \
+	            --datarootdir=/usr/share \
+	            $CONFIGURE_LIBRARY_FLAGS
+	$MAKE
+}
+
+file_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/file/README"
+	install -m 644 ChangeLog "$1/usr/share/doc/file/ChangeLog"
+	install -m 644 AUTHORS "$1/usr/share/doc/file/AUTHORS"
+	install -m 644 COPYING "$1/usr/share/doc/file/COPYING"
+}
