da134f7b770243745786ff86fb69d2460d367749
Dima Krasner <dima@dimakrasner.com>
2014-07-12 13:29:42 +0300

    Added Snownews.

---
+++ packages/snownews.sh
@@ -0,0 +1,24 @@
+PACKAGE_VERSION="1.5.12"
+PACKAGE_SOURCES="https://kiza.eu/media/software/snownews/snownews-$PACKAGE_VERSION.tar.gz"
+PACKAGE_DESC="A news reader"
+
+snownews_build() {
+	[ -d snownews-$PACKAGE_VERSION ] && rm -rf snownews-$PACKAGE_VERSION
+	tar -xzvf snownews-$PACKAGE_VERSION.tar.gz
+	cd snownews-$PACKAGE_VERSION
+
+	patch -p 1 < "$BASE_DIR/patches/snownews-build.patch"
+	./configure --prefix= --disable-nls
+	EXTRA_CFLAGS="$CFLAGS" \
+	EXTRA_LDFLAGS="$LDFLAGS" \
+	$MAKE
+}
+
+snownews_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/snownews/README"
+	install -m 644 Changelog "$1/usr/share/doc/snownews/Changelog"
+	install -m 644 AUTHOR "$1/usr/share/doc/snownews/AUTHOR"
+	install -m 644 CREDITS "$1/usr/share/doc/snownews/CREDITS"
+	install -m 644 COPYING "$1/usr/share/doc/snownews/COPYING"
+}
