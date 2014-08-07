67c6e8863e9b4719dbb409ea133b518b961c8a1a
Dima Krasner <dima@dimakrasner.com>
2014-08-07 22:23:05 +0300

    Added tcpdump.

---
+++ packages/libpcap.sh
@@ -0,0 +1,24 @@
+PACKAGE_VERSION="1.6.1"
+PACKAGE_SOURCES="http://www.tcpdump.org/release/libpcap-$PACKAGE_VERSION.tar.gz"
+PACKAGE_DESC="A packet capture library"
+
+libpcap_build() {
+	[ -d libpcap-$PACKAGE_VERSION ] && rm -rf libpcap-$PACKAGE_VERSION
+	tar -xzvf libpcap-$PACKAGE_VERSION.tar.gz
+	cd libpcap-$PACKAGE_VERSION
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --includedir=/usr/include \
+	            --datarootdir=/usr/share \
+	            $CONFIGURE_LIBRARY_FLAGS
+	$MAKE
+}
+
+libpcap_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/libpcap/README"
+	install -m 644 CHANGES "$1/usr/share/doc/libpcap/CHANGES"
+	install -m 644 CREDITS "$1/usr/share/doc/libpcap/CREDITS"
+	install -m 644 LICENSE "$1/usr/share/doc/libpcap/LICENSE"
+}
---
+++ packages/tcpdump.sh
@@ -0,0 +1,27 @@
+PACKAGE_VERSION="4.6.1"
+PACKAGE_SOURCES="http://www.tcpdump.org/release/tcpdump-$PACKAGE_VERSION.tar.gz"
+PACKAGE_DESC="A packet capture and analysis tool"
+
+tcpdump_build() {
+	[ -d tcpdump-$PACKAGE_VERSION ] && rm -rf tcpdump-$PACKAGE_VERSION
+	tar -xzvf tcpdump-$PACKAGE_VERSION.tar.gz
+	cd tcpdump-$PACKAGE_VERSION
+
+	patch -p 1 < "$BASE_DIR/patches/tcpdump-binary.patch"
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --sbindir=/bin \
+	            --datarootdir=/usr/share \
+	            --enable-ipv6 \
+	            --with-crypto
+	$MAKE
+}
+
+tcpdump_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README.md "$1/usr/share/doc/tcpdump/README.md"
+	install -m 644 CHANGES "$1/usr/share/doc/tcpdump/CHANGES"
+	install -m 644 CREDITS "$1/usr/share/doc/tcpdump/CREDITS"
+	install -m 644 LICENSE "$1/usr/share/doc/tcpdump/LICENSE"
+}
