e22576445acbeca9984694a5d6453387957ba653
Dima Krasner <dima@dimakrasner.com>
2014-08-15 17:19:36 +0300

    Added shus.

---
+++ packages/shus.sh
@@ -0,0 +1,15 @@
+PACKAGE_VERSION="git$(date +%d%m%Y)"
+PACKAGE_SOURCES="https://github.com/dimkr/shus/archive/master.zip,shus-$PACKAGE_VERSION.zip"
+PACKAGE_DESC="A web server"
+
+shus_build() {
+	[ -d shus-master ] && rm -rf shus-master
+	unzip shus-$PACKAGE_VERSION.zip
+	cd shus-master
+
+	$MAKE
+}
+
+shus_package() {
+	$MAKE DESTDIR="$1" SBIN_DIR="/bin" install
+}
