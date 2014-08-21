a74cc7329064979ec3d3147c041b67151e8ebb13
Dima Krasner <dima@dimakrasner.com>
2014-08-21 23:23:48 +0300

    Updated GNU Screen.

--- packages/screen.sh
+++ packages/screen.sh
@@ -1,13 +1,11 @@
-PACKAGE_VERSION="4.0.3"
-PACKAGE_SOURCES="http://ftp.gnu.org/gnu/screen/screen-$PACKAGE_VERSION.tar.gz https://raw.githubusercontent.com/sabotage-linux/sabotage/master/KEEP/screen-execvpe.patch"
+PACKAGE_VERSION="4.2.1"
+PACKAGE_SOURCES="http://ftp.gnu.org/gnu/screen/screen-$PACKAGE_VERSION.tar.gz"
 PACKAGE_DESC="A terminal multiplexer"
 
 screen_build() {
 	[ -d screen-$PACKAGE_VERSION ] && rm -rf screen-$PACKAGE_VERSION
 	tar -xzvf screen-$PACKAGE_VERSION.tar.gz
 	cd screen-$PACKAGE_VERSION
-
-	patch -p 1 < ../screen-execvpe.patch
 
 	./configure --host=$HOST \
 	            --prefix= \
@@ -16,7 +14,7 @@ screen_build() {
 	            --mandir=/usr/share/man \
 	            --disable-socket-dir \
 	            --disable-pam \
-	            --disable-locale \
+	            --disable-use-locale \
 	            --disable-telnet \
 	            --disable-colors256 \
 	            --disable-rxvt_osc \
@@ -30,7 +28,7 @@ screen_package() {
 	rm -f "$1/bin/screen"
 	mv "$1/bin/screen-$PACKAGE_VERSION" "$1/bin/screen"
 	install -D -m 644 README "$1/usr/share/doc/screen/README"
-	install -D -m 644 NEWS "$1/usr/share/doc/screen/NEWS"
-	install -D -m 644 ChangeLog "$1/usr/share/doc/screen/ChangeLog"
-	install -D -m 644 COPYING "$1/usr/share/doc/screen/COPYING"
+	install -m 644 NEWS "$1/usr/share/doc/screen/NEWS"
+	install -m 644 ChangeLog "$1/usr/share/doc/screen/ChangeLog"
+	install -m 644 COPYING "$1/usr/share/doc/screen/COPYING"
 }
