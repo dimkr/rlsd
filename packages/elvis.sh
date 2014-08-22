0292788f641c94b366be8be2712fc92674cf6645
Dima Krasner <dima@dimakrasner.com>
2014-08-22 19:45:15 +0300

    Replaced Debian's elvis-tiny with a minimal version of the upstream package.

--- packages/elvis.sh
+++ packages/elvis.sh
@@ -1,27 +1,26 @@
-PACKAGE_VERSION="1.4"
-PATCH_VERSION="23"
-PACKAGE_SOURCES="http://ftp.de.debian.org/debian/pool/main/e/elvis-tiny/elvis-tiny_$PACKAGE_VERSION.orig.tar.gz http://ftp.de.debian.org/debian/pool/main/e/elvis-tiny/elvis-tiny_$PACKAGE_VERSION-$PATCH_VERSION.debian.tar.gz"
+PACKAGE_VERSION="git$(date +%d%m%Y)"
+PACKAGE_SOURCES="https://github.com/mbert/elvis/archive/master.zip,elvis-$PACKAGE_VERSION.zip"
 PACKAGE_DESC="A text editor"
 
 elvis_build() {
-	[ -d elvis-tiny-$PACKAGE_VERSION.orig ] && rm -rf elvis-tiny-$PACKAGE_VERSION.orig
-	tar -xzvf elvis-tiny_$PACKAGE_VERSION.orig.tar.gz
-	cd elvis-tiny-$PACKAGE_VERSION.orig
-	tar -xzvf ../elvis-tiny_$PACKAGE_VERSION-$PATCH_VERSION.debian.tar.gz
-
-	cat debian/patches/series | while read i
-	do
-		patch -p 1 < debian/patches/$i
-	done
+	[ -d elvis-master ] && rm -rf elvis-master
+	unzip elvis-$PACKAGE_VERSION.zip
+	cd elvis-master
 
 	patch -p 1 < "$BASE_DIR/patches/elvis-build.patch"
 
-	$MAKE -f Makefile.mix
+	./configure --bindir="/bin" \
+	            --datadir="/usr/share/elvis" \
+	            --without-x \
+	            --without-gnome \
+	            --libs="-lncurses $LDFLAGS"
+	$MAKE elvis
 }
 
 elvis_package() {
 	install -D -m 755 elvis "$1/bin/elvis"
 	ln -s elvis "$1/bin/vi"
 	install -D -m 644 doc/elvis.man "$1/usr/share/man/man1/elvis.1"
-	install -D -m 644 README "$1/usr/share/doc/elvis/README"
+	install -D -m 644 COPYING "$1/usr/share/doc/elvis/COPYING"
+	install -m 644 doc/license.html "$1/usr/share/doc/elvis/license.html"
 }
