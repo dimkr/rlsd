c6417f5ad0710b8f509143e7f3f52eea88d99b13
Dima Krasner <dima@dimakrasner.com>
2014-08-23 13:00:31 +0300

    Bug fix - ca-certificates doesn't update on daily basis.

--- packages/ca_certificates.sh
+++ packages/ca_certificates.sh
@@ -1,9 +1,9 @@
 PACKAGE_VERSION="git$(date +%d%m%Y)"
-PACKAGE_SOURCES="https://raw.githubusercontent.com/bagder/curl/master/lib/mk-ca-bundle.pl,mk-ca-bundle-$PACKAGTE_VERSION.pl http://mxr.mozilla.org/mozilla/source/security/nss/COPYING?raw=1,COPYING"
+PACKAGE_SOURCES="https://raw.githubusercontent.com/bagder/curl/master/lib/mk-ca-bundle.pl,mk-ca-bundle-$PACKAGE_VERSION.pl http://mxr.mozilla.org/mozilla/source/security/nss/COPYING?raw=1,COPYING"
 PACKAGE_DESC="CA certificates"
 
 ca_certificates_build() {
-	perl mk-ca-bundle-$PACKAGTE_VERSION.pl cert.pem
+	perl mk-ca-bundle-$PACKAGE_VERSION.pl cert.pem
 }
 
 ca_certificates_package() {
