dd55fb364feea9151629e7665bd750518a027a67
Dima Krasner <dima@dimakrasner.com>
2014-09-02 21:45:42 +0300

    Bug fix - the kernel headers build script ignores KARCH.

--- packages/linux_headers.sh
+++ packages/linux_headers.sh
@@ -6,12 +6,12 @@ linux_headers_build() {
 	[ -d linux-$PACKAGE_VERSION ] && rm -rf linux-$PACKAGE_VERSION
 	tar -xJvf linux-libre-$PACKAGE_VERSION-gnu1.tar.xz
 	cd linux-$PACKAGE_VERSION
-	$MAKE clean
-	$MAKE mrproper
+	$MAKE ARCH="$KARCH" clean
+	$MAKE ARCH="$KARCH" mrproper
 }
 
 linux_headers_package() {
-	$MAKE ARCH="$KARCH" INSTALL_HDR_PATH="$1" headers_install
+	ARCH="$KARCH" $MAKE INSTALL_HDR_PATH="$1" headers_install
 	mkdir "$1/usr"
 	mv "$1/include" "$1/usr/"
 	find "$1/usr/include" -name .install -or -name ..install.cmd | xargs rm -f
