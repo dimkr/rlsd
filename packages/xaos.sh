1687d513002a50650a3cda1b14332b1a4a81095a
Dima Krasner <dima@dimakrasner.com>
2014-09-06 15:07:00 +0300

    Fixed building XaoS for x86.

--- packages/xaos.sh
+++ packages/xaos.sh
@@ -1,11 +1,17 @@
 PACKAGE_VERSION="3.6"
-PACKAGE_SOURCES="http://sourceforge.net/projects/xaos/files/XaoS/$PACKAGE_VERSION/xaos-$PACKAGE_VERSION.tar.gz"
+PACKAGE_SOURCES="http://sourceforge.net/projects/xaos/files/XaoS/$PACKAGE_VERSION/xaos-$PACKAGE_VERSION.tar.gz https://projects.archlinux.org/svntogit/packages.git/plain/trunk/xaos-3.5-build-fix-i686.patch?h=packages/xaos,xaos-3.5-build-fix-i686.patch"
 PACKAGE_DESC="A fractal zoomer"
 
 xaos_build() {
 	[ -d xaos-$PACKAGE_VERSION ] && rm -rf xaos-$PACKAGE_VERSION
 	tar -xzvf xaos-$PACKAGE_VERSION.tar.gz
 	cd xaos-$PACKAGE_VERSION
+
+	case "$ARCH" in
+		i?86)
+			patch -p 1 < ../xaos-3.5-build-fix-i686.patch
+			;;
+	esac
 
 	./configure --host=$HOST \
 	            --prefix= \
