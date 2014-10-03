3f11656d736d00bb85b83ee16c38d8f8640ab572
Dima Krasner <dima@dimakrasner.com>
2014-10-04 00:31:14 +0300

    Added Mutt and msmtp.

---
+++ packages/msmtp.sh
@@ -0,0 +1,28 @@
+PACKAGE_VERSION="1.4.32"
+PACKAGE_SOURCES="http://sourceforge.net/projects/msmtp/files/msmtp/$PACKAGE_VERSION/msmtp-$PACKAGE_VERSION.tar.bz2"
+PACKAGE_DESC="An e-mail sending tool"
+
+msmtp_build() {
+	[ -d msmtp-$PACKAGE_VERSION ] && rm -rf msmtp-$PACKAGE_VERSION
+	tar -xjvf msmtp-$PACKAGE_VERSION.tar.bz2
+	cd msmtp-$PACKAGE_VERSION
+
+	./configure --host=$HOST \
+	            --prefix= \
+	            --datarootdir=/usr/share \
+	            --disable-nls \
+	            --with-ssl=openssl \
+	            --without-libidn
+	$MAKE
+}
+
+msmtp_package() {
+	$MAKE DESTDIR="$1" install
+	install -D -m 644 README "$1/usr/share/doc/msmtp/README"
+	install -m 644 FAQ "$1/usr/share/doc/msmtp/FAQ"
+	install -m 644 NEWS "$1/usr/share/doc/msmtp/NEWS"
+	install -m 644 ChangeLog "$1/usr/share/doc/msmtp/ChangeLog"
+	install -m 644 AUTHORS "$1/usr/share/doc/msmtp/AUTHORS"
+	install -m 644 THANKS "$1/usr/share/doc/msmtp/THANKS"
+	install -m 644 COPYING "$1/usr/share/doc/msmtp/COPYING"
+}
---
+++ packages/mutt.sh
@@ -0,0 +1,24 @@
+PACKAGE_VERSION="1.5.23"
+PACKAGE_SOURCES="http://sourceforge.net/projects/mutt/files/mutt/mutt-$PACKAGE_VERSION.tar.gz"
+PACKAGE_DESC="An e-mail client"
+
+mutt_build() {
+	[ -d mutt-$PACKAGE_VERSION ] && rm -rf mutt-$PACKAGE_VERSION
+	tar -xzvf mutt-$PACKAGE_VERSION.tar.gz
+	cd mutt-$PACKAGE_VERSION
+
+	SENDMAIL="/bin/msmtp" \
+	./configure --host=$HOST \
+	            --prefix= \
+	            --datarootdir=/usr/share \
+	            --enable-pop \
+	            --enable-imap \
+	            --enable-smtp \
+	            --disable-nls \
+	            --with-ssl
+	$MAKE
+}
+
+mutt_package() {
+	$MAKE DESTDIR="$1" install
+}
