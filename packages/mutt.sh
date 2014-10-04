0c6ca7f4494e9242636f8b36a530cdb31ce79e84
Dima Krasner <dima@dimakrasner.com>
2014-10-04 13:47:23 +0300

    Enabled Mutt's header cache.

--- packages/mutt.sh
+++ packages/mutt.sh
@@ -13,6 +13,7 @@ mutt_build() {
 	            --datarootdir=/usr/share \
 	            --enable-pop \
 	            --enable-imap \
+	            --enable-hcache \
 	            --disable-nls \
 	            --with-ssl
 	$MAKE
