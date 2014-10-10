ead01ce8db01f3dfd6b1068727ace397a380da84
Dima Krasner <dima@dimakrasner.com>
2014-10-10 12:23:54 +0300

    Added Debian's dhcpcd 3.2.3 patches.
    This takes care of CVE-2012-2152 among other issues.

--- packages/dhcpcd.sh
+++ packages/dhcpcd.sh
@@ -1,5 +1,5 @@
 PACKAGE_VERSION="3.2.3"
-PACKAGE_SOURCES="http://roy.marples.name/downloads/dhcpcd/dhcpcd-$PACKAGE_VERSION.tar.bz2"
+PACKAGE_SOURCES="http://roy.marples.name/downloads/dhcpcd/dhcpcd-$PACKAGE_VERSION.tar.bz2 http://ftp.de.debian.org/debian/pool/main/d/dhcpcd/dhcpcd_3.2.3-11.diff.gz"
 PACKAGE_DESC="A DHCP client"
 
 dhcpcd_build() {
@@ -7,6 +7,7 @@ dhcpcd_build() {
 	tar -xjvf dhcpcd-$PACKAGE_VERSION.tar.bz2
 	cd dhcpcd-$PACKAGE_VERSION
 
+	cat ../dhcpcd_3.2.3-11.diff.gz | gzip -d | patch -p 1
 	patch -p 1 < "$BASE_DIR/patches/dhcpcd-musl.patch"
 
 	$MAKE CC="$CC" CFLAGS="-Icompat $CFLAGS" LDFLAGS="$LDFLAGS" INFODIR="/run"
