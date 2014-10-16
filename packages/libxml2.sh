2f1104cbb68ce28b6c7626ccd537c865909165d7
Dima Krasner <dima@dimakrasner.com>
2014-10-16 20:21:04 +0300

    Updated libxml2.

--- packages/libxml2.sh
+++ packages/libxml2.sh
@@ -1,4 +1,4 @@
-PACKAGE_VERSION="2.9.1"
+PACKAGE_VERSION="2.9.2"
 PACKAGE_SOURCES="ftp://ftp.xmlsoft.org/libxml2/libxml2-$PACKAGE_VERSION.tar.gz"
 PACKAGE_DESC="A XML parsing library"
 
@@ -7,6 +7,9 @@ libxml2_build() {
 	tar -xzvf libxml2-$PACKAGE_VERSION.tar.gz
 	cd libxml2-$PACKAGE_VERSION
 
+	patch -p 1 < "$BASE_DIR/patches/libxml2-doc.patch"
+	patch -p 1 < "$BASE_DIR/patches/libxml2-examples.patch"
+	patch -p 1 < "$BASE_DIR/patches/libxml2-cmake.patch"
 	patch -p 1 < "$BASE_DIR/patches/libxml2-config.patch"
 
 	./configure --host=$HOST \
@@ -25,11 +28,9 @@ libxml2_build() {
 
 libxml2_package() {
 	$MAKE DESTDIR="$1" install
-	mv "$1/usr/share/doc/libxml2-$PACKAGE_VERSION" "$1/usr/share/doc/libxml2"
-	install -D -m 644 README "$1/usr/share/doc/libxml2/README"
+	install -m 644 README "$1/usr/share/doc/libxml2/README"
 	install -m 644 ChangeLog "$1/usr/share/doc/libxml2/ChangeLog"
 	install -m 644 NEWS "$1/usr/share/doc/libxml2/NEWS"
 	install -m 644 AUTHORS "$1/usr/share/doc/libxml2/AUTHORS"
-	install -m 644 Copyright "$1/usr/share/doc/libxml2/Copyright"
 	install -m 644 COPYING "$1/usr/share/doc/libxml2/COPYING"
 }
