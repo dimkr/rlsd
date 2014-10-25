96e0666dbbf4daa551c9f8ffbfb44717c861c5ef
Dima Krasner <dima@dimakrasner.com>
2014-10-25 12:33:11 +0300

    Replaced underscores in package names with hypens.

--- packages/linux-headers.sh
+++ packages/linux-headers.sh
@@ -15,8 +15,8 @@ package() {
 	mkdir "$1/usr"
 	mv "$1/include" "$1/usr/"
 	find "$1/usr/include" -name .install -or -name ..install.cmd | xargs rm -f
-	install -D -m 644 README "$1/usr/share/doc/linux_headers/README"
-	install -m 644 COPYING "$1/usr/share/doc/linux_headers/COPYING"
-	install -m 644 CREDITS "$1/usr/share/doc/linux_headers/CREDITS"
-	install -m 644 MAINTAINERS "$1/usr/share/doc/linux_headers/MAINTAINERS"
+	install -D -m 644 README "$1/usr/share/doc/linux-headers/README"
+	install -m 644 COPYING "$1/usr/share/doc/linux-headers/COPYING"
+	install -m 644 CREDITS "$1/usr/share/doc/linux-headers/CREDITS"
+	install -m 644 MAINTAINERS "$1/usr/share/doc/linux-headers/MAINTAINERS"
 }
