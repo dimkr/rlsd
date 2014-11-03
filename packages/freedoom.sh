5bb847ad9e131ef810c26ce9b16107868692d2a7
Dima Krasner <dima@dimakrasner.com>
2014-11-03 20:36:54 +0200

    Updated Fredoom.

--- packages/freedoom.sh
+++ packages/freedoom.sh
@@ -1,17 +1,18 @@
-PACKAGE_VERSION="0.8"
-PACKAGE_SOURCES="https://github.com/freedoom/freedoom/releases/download/v$PACKAGE_VERSION/freedoom-ultimate-v$PACKAGE_VERSION.zip"
+PACKAGE_VERSION="0.9"
+PACKAGE_SOURCES="https://github.com/freedoom/freedoom/releases/download/v$PACKAGE_VERSION/freedoom-$PACKAGE_VERSION.zip"
 PACKAGE_DESC="Game content for the Doom engine"
 PACKAGE_ARCH="all"
 
 build() {
-	[ -d freedoom-ultimate-v$PACKAGE_VERSION ] && rm -rf freedoom-ultimate-v$PACKAGE_VERSION
-	unzip freedoom-ultimate-v$PACKAGE_VERSION.zip
-	cd freedoom-ultimate-v$PACKAGE_VERSION
-
+	[ -d freedoom-$PACKAGE_VERSION ] && rm -rf freedoom-$PACKAGE_VERSION
+	unzip freedoom-$PACKAGE_VERSION.zip
+	cd freedoom-$PACKAGE_VERSION
 }
 
 package() {
-	install -D -m 644 doom.wad "$1/usr/share/doom/doom.wad"
+	install -D -m 644 freedoom1.wad "$1/usr/share/doom/freedoom1.wad"
+	install -m 644 freedoom2.wad "$1/usr/share/doom/freedoom2.wad"
+	ln -s freedoom1.wad "$1/usr/share/doom/doom.wad"
 	install -D -m 644 README.html "$1/usr/share/doc/freedoom/README.html"
 	install -m 644 CREDITS "$1/usr/share/doc/freedoom/CREDITS"
 	install -m 644 COPYING "$1/usr/share/doc/freedoom/COPYING"
