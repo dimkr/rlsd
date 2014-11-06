b50e9fab16ba3c4a7edc2b33a66872441a5410d6
Dima Krasner <dima@dimakrasner.com>
2014-11-06 22:43:25 +0200

    Made the SQLite version human-readable.

--- packages/sqlite.sh
+++ packages/sqlite.sh
@@ -1,11 +1,12 @@
-PACKAGE_VERSION="3080701"
-PACKAGE_SOURCES="http://www.sqlite.org/2014/sqlite-autoconf-$PACKAGE_VERSION.tar.gz https://projects.archlinux.org/svntogit/packages.git/plain/trunk/license.txt?h=packages/sqlite,license.txt"
+PACKAGE_VERSION="3.8.7.1"
+PACKAGE_REAL_VERSION="3080701"
+PACKAGE_SOURCES="http://www.sqlite.org/2014/sqlite-autoconf-$PACKAGE_REAL_VERSION.tar.gz https://projects.archlinux.org/svntogit/packages.git/plain/trunk/license.txt?h=packages/sqlite,license.txt"
 PACKAGE_DESC="An embedded database"
 
 build() {
-	[ -d sqlite-autoconf-$PACKAGE_VERSION ] && rm -rf sqlite-autoconf-$PACKAGE_VERSION
-	tar -xzvf sqlite-autoconf-$PACKAGE_VERSION.tar.gz
-	cd sqlite-autoconf-$PACKAGE_VERSION
+	[ -d sqlite-autoconf-$PACKAGE_REAL_VERSION ] && rm -rf sqlite-autoconf-$PACKAGE_REAL_VERSION
+	tar -xzvf sqlite-autoconf-$PACKAGE_REAL_VERSION.tar.gz
+	cd sqlite-autoconf-$PACKAGE_REAL_VERSION
 
 	./configure --host=$HOST \
 	            --prefix= \
