fc2486604338ab5acbdeeab814acfa7ce67c9d70
Dima Krasner <dima@dimakrasner.com>
2014-11-19 20:07:58 +0200

    Updated SQLite.

--- packages/sqlite.sh
+++ packages/sqlite.sh
@@ -1,5 +1,5 @@
-PACKAGE_VERSION="3.8.7.1"
-PACKAGE_REAL_VERSION="3080701"
+PACKAGE_VERSION="3.8.7.2"
+PACKAGE_REAL_VERSION="3080702"
 PACKAGE_SOURCES="http://www.sqlite.org/2014/sqlite-autoconf-$PACKAGE_REAL_VERSION.tar.gz https://projects.archlinux.org/svntogit/packages.git/plain/trunk/license.txt?h=packages/sqlite,license.txt"
 PACKAGE_DESC="An embedded database"
 
