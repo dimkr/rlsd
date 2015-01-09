76f3b78fc0209c0fe3bd8ef4835ced57ed6071bd
Dima Krasner <dima@dimakrasner.com>
2015-01-09 18:18:18 +0200

    Updated libjpeg-turbo.

--- packages/libjpeg-turbo.sh
+++ packages/libjpeg-turbo.sh
@@ -1,4 +1,4 @@
-PACKAGE_VERSION="1.3.1"
+PACKAGE_VERSION="1.4.0"
 PACKAGE_SOURCES="http://sourceforge.net/projects/libjpeg-turbo/files/$PACKAGE_VERSION/libjpeg-turbo-$PACKAGE_VERSION.tar.gz"
 PACKAGE_DESC="A library for handling JPEG images"
 
